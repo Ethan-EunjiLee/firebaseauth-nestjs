4c9ed453863a8c6ca0d16855cf9e05ea4cb23622
DHinSL <hyeon.dh.kim>
2021-05-20 14:12:12 +0900

    passport-kakao fail because validate

--- src/strategy/KakaoStrategy.ts
+++ src/strategy/KakaoStrategy.ts
@@ -24,7 +24,9 @@ export class KakaoStrategy{
             clientID: '974d06dc1d9c335cc6d0fcd94ee7703a', // Rest API 키
             clientSecret: 'nywcIy2P3jLbAwRkq4XG2rI6eVCEX5Kf',
             callbackURL: 'http://localhost:3001/kakaoRedirect'
-        }, async (accessToken: string, refreshToken: string, profile: any, done: Function) => {
+        }, (accessToken: string, refreshToken: string, profile: any, done: Function) => {
+            const profile_json = profile._json;
+            console.log('json데이터 확인: ', profile_json);
             console.log('hi');
             return done (null, profile);
         }))
